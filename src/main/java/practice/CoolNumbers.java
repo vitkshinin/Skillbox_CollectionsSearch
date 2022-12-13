49225dc94f998bc2a5613c682d9486519339f1a5
Vitaly Kshinin <vitaly.kshinin@gmail.com>
2022-12-13 04:05:45 +0300

    Homework 10.13 done

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,30 +1,93 @@
 package practice;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        List<String> coolNumbersCollection = new ArrayList<>();
+        for (int i = 0; i <= 2_000_000; i++) {
+            coolNumbersCollection.add(getFullNumber());
+        }
+        Collections.sort(coolNumbersCollection);
+        return coolNumbersCollection;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        long time = System.nanoTime();
+        if (list.contains(number)) {
+            System.out.println("Поиск перебором: номер найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
+            return true;
+        }
+        System.out.println("Поиск перебором: номер не найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
+        long time = System.nanoTime();
+        int index = Collections.binarySearch(sortedList, number);
+        if (index >= 0) {
+            System.out.println("Бинарный поиск: номер найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
+            return true;
+        }
+        System.out.println("Бинарный поиск: номер не найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
         return false;
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        HashSet<String> arrNumber = new HashSet<>(hashSet);
+        long time = System.nanoTime();
+        if (arrNumber.contains(number)) {
+            System.out.println("Поиск в HashSet: номер найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
+            return true;
+        }
+        System.out.println("Поиск в HashSet: номер не найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
         return false;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        TreeSet<String> sortCoolNumber = new TreeSet<>(treeSet);
+        long time = System.nanoTime();
+        if (sortCoolNumber.contains(number)) {
+            System.out.println("Поиск в TreeSet: номер найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
+            return true;
+        }
+        System.out.println("Поиск в TreeSet: номер не найден, поиск занял " + (double) (System.nanoTime() - time) + " нс");
         return false;
+    }
+
+    public static String getLetter() {
+        ArrayList<String> characters = new ArrayList<>
+                (Arrays.asList("А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"));
+        double max = characters.size();
+        double randomCharacters = (Math.random()) * max;
+        return characters.get((int) randomCharacters);
+    }
+
+    private static String getNumbers() {
+        ArrayList<String> numbers = new ArrayList<>
+                (Arrays.asList("111", "222", "333", "444", "555", "666", "777", "888", "999"));
+        double max = numbers.size();
+        double randomCharacter = (Math.random()) * max;
+        return numbers.get((int) randomCharacter);
+    }
+
+    private static String getRegion() {
+        double max = 199;
+        int randomRegion = (int) (((Math.random()) * max) + 1);
+        String region = "" + randomRegion;
+        if (randomRegion <= 9) {
+            region = "0" + randomRegion;
+        }
+        return region;
+    }
+
+    public static String getFullNumber() {
+        return CoolNumbers.getLetter() +
+                CoolNumbers.getNumbers() +
+                CoolNumbers.getLetter() +
+                CoolNumbers.getLetter() +
+                CoolNumbers. getRegion();
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.*;
+
 public class Main {
     /*
     TODO:
@@ -9,6 +11,20 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> coolNumber = new ArrayList<>(CoolNumbers.generateCoolNumbers());
+        HashSet<String> coolNumberHashSet = new HashSet<>(CoolNumbers.generateCoolNumbers());
+        TreeSet<String> coolNumberTreeSet = new TreeSet<>(CoolNumbers.generateCoolNumbers());
+        System.out.println("Количество номеров в базе: " + coolNumber.size());
+
+        while (true) {
+            System.out.println("Введите номер для поиска в формате, например: A111BC077");
+            String inputNumber = scanner.nextLine();
+            System.out.println(CoolNumbers.bruteForceSearchInList(coolNumber, inputNumber));
+            System.out.println(CoolNumbers.binarySearchInList(coolNumber, inputNumber));
+            System.out.println(CoolNumbers.searchInHashSet(coolNumberHashSet, inputNumber));
+            System.out.println(CoolNumbers.searchInTreeSet(coolNumberTreeSet, inputNumber));
+        }
 
     }
 }
